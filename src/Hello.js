030a0bb06da5fbf8af6fcb435765bbd98a4bfac8
VarshaMani17 <varshatheeran@gmail.com>
2024-01-23 16:04:13 +0530

    version1

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function Hello(){
+    return(
+        <div>
+        <h2 align="center">A powerful online engagement tool thats intuitive and simple to use.</h2>
+        <h4 align="center">With stellar one-click reports and unmatched support, see how Circle will make a difference in your business.</h4>
+        <h3>Get Started Free</h3>
+        <p>Affiliated with Anna University, Chennai, the Kongu Engineering College's 167-acre campus is located near Perundurai, outside Erode, about 17 kilometres from Erode Railway Station. It offers bachelor's, master's and PhD degrees in a number of disciplines.</p>
+        </div>
+    )
+}
