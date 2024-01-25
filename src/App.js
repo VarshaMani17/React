ffeb6dcb7b14fbe99e3659e27a8769e0d904a092
VarshaMani17 <varshatheeran@gmail.com>
2024-01-25 09:38:43 +0530

    version

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import Hello from './Hello'
-import Myclass from './Myclass'
-import Ter from './Ter'
+import {BrowserRouter, Routes, Route } from "react-router-dom"
+import Layout from "./Layout"
+import Home from "./Home"
+import About from "./About"
+import Contact from "./Contact"
+
 export default function App() { 
   return (
-    <div>
-      <Hello/>
-      <Myclass/>
-      <Ter/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="contact" element={<Contact/>}/>
+
+      </Route>
+    </Routes>
+    </BrowserRouter>
   )
   }
 
